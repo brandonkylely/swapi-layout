4fae71094ab5e772a693406cc21a5e22d58a8d14
Brandon Ly <brandonkly@ucla.edu>
2024-02-21 16:31:38 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Personal React Project Starter
+
+### Includes
+- Vite
+- React
+- TailwindCSS
+- Daisy UI
+- Jotai
+- React Router
---
+++ src/api/index.js
@@ -0,0 +1,22 @@
+/**
+ * @placeholderURL API endpoints
+ * /posts	100 posts
+ * /comments	500 comments
+ * /albums	100 albums
+ * /photos	5000 photos
+ * /todos	200 todos
+ * /users	10 users
+ */
+export const placeholderURL = "https://jsonplaceholder.typicode.com";
+
+
+/**
+ * films string -- The URL root for Film resources
+ * people string -- The URL root for People resources
+ * planets string -- The URL root for Planet resources
+ * species string -- The URL root for Species resources
+ * starships string -- The URL root for Starships resources
+ * vehicles string -- The URL root for Vehicles resources
+ */
+
+export const swapiURL = "https://swapi.dev/api"
